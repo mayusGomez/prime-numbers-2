72aaab194aa6edc73fb1fe4e88db9df20decd334
Alexander Gomez <alexander.gomez.higuita@gmail.com>
2018-10-25 01:36:12 -0500

    Fix for optimize generator function and obly evaluate numbers of the form 6k+-1

--- README.md
+++ README.md
@@ -3,20 +3,46 @@ List of Prime Numbers
 
 This package list all the prime numbers between 2 to a parameter value, by default 100.
 This is implemented like another option to next_prime() function of [gmpy2](https://gmpy2.readthedocs.io/en/latest/intro.html) 
-library
+library.
+
+The function next_prime(max_number_eval) is a generator, only search primes of the form 6k ± 1. The prime numbers are not 
+saved, then for evaluate if a number is prime, search for factors of the form 6k ± 1 and less than sqrt(max_number_eval)
 
 
 Run
 ---
 For execute this code:
-
-```python
-    from prime_numbers import prime_numbers
-    prime_numbers()    
+- Create VirtualEnv and Install requirements
 ```
+$ virtualenv env
+$ source env/bin/activate
+$ pip install -r requirements.txt
+```
+- From python3 console
+```python
+from prime_number import prime_numbers
+prime_numbers()    
+```
+- Can run as a generator too
+```python
+from prime_number import next_prime
+for x in iter(next_prime(max_number_eval=100)):
+   print(x)
+```
+
+Test
+----
+```
+$ pytest
+```
+
+Requirements
+------------
+- python 3+
+- pytest
 
 References
 ----------
 
--   Wikipedia [Primary Test](https://en.wikipedia.org/wiki/Primality_test) resource
-
+-   Wikipedia [Primary Test](https://en.wikipedia.org/wiki/Primality_test) 
+-   Wikipedia [Sieve of Eratosthenes](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes) 
--- prime_number/prime_number.py
+++ prime_number/prime_number.py
@@ -9,8 +9,9 @@ def eval_prim(possible_prime):
     This algorithm take on account that:
     - Prime number is natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers
     - A simple approach is evaluate "possible_prime % range(2,possible_prime)==0" but:
-        - The factors to evaluate are 2 to sqrt(possible_prime)
-        - All primes are of the form 6k ± 1
+        - The factors to evaluate are 2 to sqrt(possible_prime),
+        - The factors to evaluate only must be primes, primes are of the form 6k ± 1
+        - Not use Sieve of Eratosthenes(https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes) for save memory
 
     :param possible_prime: number to evaluate
     :return: True or False
@@ -19,31 +20,43 @@ def eval_prim(possible_prime):
         return False
     elif possible_prime <= 3:
         return True
-    elif possible_prime % 2 == 0 or possible_prime % 3 == 0:
+    elif possible_prime % 2 == 0 or possible_prime % 3 == 0:  # This values are primes
         return False
 
-    i = 5
+    i = 5  # first factor to evaluate, of the form 6(1)-1, second to evaluate is 7 of teh form 6(1)+1
     while i**2 <= possible_prime:
         if possible_prime % i == 0 or possible_prime % (i + 2) == 0:
             return False
-        i += 6
+        i += 6 # increment 6 for evaluate next possible prime factors
     return True
 
 
-def next_prime(max_number_eval):
+def next_prime(max_number_eval=100):
     """Generate the prime values from 2 to a max number
 
     Conditions:
-    - Exception of 2, a prime number is not even, only evaluate not even values from 3
+    - 2 and 3 are prime, next only evaluate number of the form 6k ± 1
+    - Start at 5 and 7, next to 11 and 13, next to 17 and 19, ...
+    - To know if a number is prime call the function prime_eval(number)
     """
     if max_number_eval <= 1:
         return
     else:
         yield 2
 
-        for x in range(3, max_number_eval+1, 2):
-            if eval_prim(x):
-                yield x
+    if max_number_eval > 2:
+        yield 3
+
+        k = 5
+        while k <= max_number_eval:
+            if eval_prim(k):
+                yield k
+
+            k += 2
+            if k <= max_number_eval and eval_prim(k):
+                yield k
+
+            k += 4
 
 
 def prime_numbers(max_number_eval=100):
@@ -58,5 +71,5 @@ def prime_numbers(max_number_eval=100):
 
 
 if __name__ == '__main__':
-    prime_numbers()
+    prime_numbers(503)
 
